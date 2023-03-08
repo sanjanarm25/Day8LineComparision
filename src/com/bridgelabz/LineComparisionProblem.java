d9736e7ebc016b97167abf907773ad5512e3ea71
sanjanamalkar <sanjanamalkar@gmail.com>
2023-03-08 12:09:27 +0530

    Added:Line comparision

---
+++ src/com/bridgelabz/LineComparisionProblem.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class LineComparisionProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line\n" +
+                "Comparison Computation\n" +
+                "Program on Master Branch");
+
+
+    }
+}
